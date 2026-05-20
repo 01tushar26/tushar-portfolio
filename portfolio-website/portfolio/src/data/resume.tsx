b1287f7f8e4727c994a09ceaff2374a0eda34880
Tushar Tyagi <tushartyagi2601@gmail.com>
2026-05-20 12:06:33 +0530

    feat : add project thumbnails

--- portfolio-website/portfolio/src/data/resume.tsx
+++ portfolio-website/portfolio/src/data/resume.tsx
@@ -160,7 +160,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "",
+      image: "/learnify.png",
       video: "",
     },
     {
